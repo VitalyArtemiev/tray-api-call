8d9a71fb1669709e86bcc682b8a476173f48b21e
Виталий Артемьев <vitaly.artemiev@yandex.ru>
2022-03-13 04:30:05 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,107 @@
+use core::mem::MaybeUninit;
+use trayicon::*;
+use winapi::um::winuser;
+
+fn main() {
+    #[derive(Copy, Clone, Eq, PartialEq, Debug)]
+    enum Events {
+        ClickTrayIcon,
+        DoubleClickTrayIcon,
+        Exit,
+        Item1,
+        Item2,
+        Item3,
+        Item4,
+        CheckItem1,
+        SubItem1,
+        SubItem2,
+        SubItem3,
+    }
+
+    let (s, r) = std::sync::mpsc::channel::<Events>();
+    let icon_off_buf = include_bytes!("../res/wireguard_off.ico");
+    let icon_on_buf = include_bytes!("../res/wireguard_on.ico");
+
+    let icon_off = Icon::from_buffer(icon_off_buf, None, None).unwrap();
+    let icon_on = Icon::from_buffer(icon_on_buf, None, None).unwrap();
+
+    // Needlessly complicated tray icon with all the whistles and bells
+    let mut tray_icon = TrayIconBuilder::new()
+        .sender(s)
+        .icon_from_buffer(icon_off_buf)
+        .tooltip("Cool Tray 👀 Icon")
+        .on_click(Events::ClickTrayIcon)
+        .on_double_click(Events::DoubleClickTrayIcon)
+        .menu(
+            MenuBuilder::new()
+                .item("Item 3 Replace Menu 👍", Events::Item3)
+                .item("Item 2 Change Icon Green", Events::Item2)
+                .item("Item 1 Change Icon Red", Events::Item1)
+                .separator()
+                .checkable("Enable tunnel", false, Events::CheckItem1)
+                .submenu(
+                    "Sub Menu",
+                    MenuBuilder::new()
+                        .item("Sub item 1", Events::SubItem1)
+                        .item("Sub Item 2", Events::SubItem2)
+                        .item("Sub Item 3", Events::SubItem3),
+                )
+                .with(MenuItem::Item {
+                    name: "Item Disabled".into(),
+                    disabled: true, // Disabled entry example
+                    id: Events::Item4,
+                    icon: None,
+                })
+                .separator()
+                .item("E&xit", Events::Exit),
+        )
+        .build()
+        .unwrap();
+
+    std::thread::spawn(move || {
+        r.iter().for_each(|m| match m {
+            Events::DoubleClickTrayIcon => {
+                println!("Double click");
+            }
+            Events::ClickTrayIcon => {
+                println!("Single click");
+            }
+            Events::Exit => {
+                println!("Please exit");
+            }
+            Events::Item1 => {
+                tray_icon.set_icon(&icon_on).unwrap();
+            }
+            Events::Item2 => {
+                tray_icon.set_icon(&icon_off).unwrap();
+            }
+            Events::Item3 => {
+                tray_icon
+                    .set_menu(
+                        &MenuBuilder::new()
+                            .item("New menu item", Events::Item1)
+                            .item("Exit", Events::Exit),
+                    )
+                    .unwrap();
+            }
+            e => {
+                println!("{:?}", e);
+            }
+        })
+    });
+
+    // Your applications message loop. Because all applications require an
+    // application loop, you are best served using an `winit` crate.
+    loop {
+        unsafe {
+            let mut msg = MaybeUninit::uninit();
+            let bret = winuser::GetMessageA(msg.as_mut_ptr(), 0 as _, 0, 0);
+            if bret > 0 {
+                winuser::TranslateMessage(msg.as_ptr());
+                winuser::DispatchMessageA(msg.as_ptr());
+            } else {
+                break;
+            }
+        }
+    }
+}
